2bed6be474be900da106ae60d0a912578d3229b9
zhanghaozhuang <zhanghaozhuang@acfun.tv>
2018-11-05 16:00:45 +0800

    babel

---
+++ es6-babel-es5/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+* author: zhanghaozhuang 
+* @file es6 转 es5
+*/
+// Babel-cli => npm install -g babel-cli
+// npm install --save-dev babel-preset-es2015 babel-cli
+// babel src/index.js -o dist/index.js
+var a = 1;
+console.log(1);
---
+++ es6-babel-es5/src/index.js
@@ -0,0 +1,10 @@
+/*
+* author: zhanghaozhuang 
+* @file es6 转 es5
+*/
+// Babel-cli => npm install -g babel-cli
+// npm install --save-dev babel-preset-es2015 babel-cli
+// babel src/index.js -o dist/index.js
+// 使用方法 cd es6-babel-es5 npm install
+let a = 1;
+console.log(1);
